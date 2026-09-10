c669185bb39b38dac25f4f1450ea95c5bd057cca
xltxlt <40884479+xltxlt@users.noreply.github.com>
2026-09-10 10:36:58 +0800

    feat(testDesigner): add json editor

---
+++ testDesigner/components/DesignerJsonEditor.vue
@@ -0,0 +1,13 @@
+<template>
+<el-input v-model="text" type="textarea" :rows="8" />
+</template>
+
+<script setup lang="ts">
+import {computed} from 'vue'
+const props=defineProps<{modelValue:any}>()
+const emit=defineEmits(['update:modelValue'])
+const text=computed({
+ get:()=>JSON.stringify(props.modelValue||{},null,2),
+ set:(v)=>{try{emit('update:modelValue',JSON.parse(v))}catch(e){}}
+})
+</script>
