1f5072d7347d8b272efa5cdd484dd01ba6456e1d
xltxlt <40884479+xltxlt@users.noreply.github.com>
2026-09-10 10:37:37 +0800

    feat(testDesigner): add designer canvas

---
+++ testDesigner/components/DesignerCanvas.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="designer-canvas">
+    <div v-if="!schema.form.length" class="empty">拖入组件开始设计</div>
+    <DesignerField
+      v-for="item in schema.form"
+      :key="item.fieldName"
+      :field="item"
+      :active="current===item"
+      @select="$emit('select',item)"
+      @remove="$emit('remove',item)"
+      @copy="$emit('copy',item)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DesignerField from './DesignerField.vue'
+
+defineProps<{schema:any,current:any}>()
+defineEmits(['select','remove','copy'])
+</script>
